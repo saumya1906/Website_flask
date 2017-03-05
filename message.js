8244595ad78c468ab1deb9007aa77c07ef708ade
Saumya Balodi <saumya15172@iiitd.ac.in>
2017-03-05 18:35:59 +0530

    Add files via upload

---
+++ message.js
@@ -0,0 +1,4 @@
+var mongoose=require('mongoose');
+var Schema=mongoose.Schema;
+var schema=new Schema({content:{type: String,required:true},user:{type: Schema.Types.ObjectId,ref:'User'}})
+module.exports=mongoose.model('Message',schema);
